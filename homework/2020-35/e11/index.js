686e210a88c414da0bc7ea6d17c7b5d626f8726e
dobby-ide <fabio_privitera@yahoo.it>
2021-09-13 11:12:24 +0300

    commit

---
+++ homework/2020-35/e11/index.js
@@ -0,0 +1,8 @@
+var cars = ["mercedes","redbull", "fiat", "ferrari", "alfa romeo", "mclaren", "aston martin"]; 
+for(var i=0; i<cars.length;i++){
+  console.log(cars[i]);
+}
+console.log();
+for(x in cars){
+  console.log(cars[x]);
+}

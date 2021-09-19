d228a6a1cf4c6e34d30e7774f03ab041ccef37af
dobby-ide <fabio_privitera@yahoo.it>
2021-09-19 09:54:16 +0300

    exercise 10 is ready

--- homework/2021-36/e10/index.js
+++ homework/2021-36/e10/index.js
@@ -1,21 +1,60 @@
-var readline = require('readline-sync');
-var asciitable = require("asciitable");
-let options = {
-  
-}
+var readlineMenu = require('readline-sync');
+var database = [];
+
 
 function add(){
+  let readlineAdd = require('readline-sync');
+  let userInput = readlineAdd.question("please write a name to be add: ");
+  database.push(userInput);
+  main();
+
 
 }
-function deleteName() {
-
+//problem: if we have same name in the array it deletes both
+function remove() {
+  let readlineDelete = require('readline-sync');
+  let userInput = readlineDelete.question('please write a name to be deleted ');
+  database.forEach(function(item, index, array) {
+if(userInput == item){
+  let pos = database.indexOf(item);
+  database.splice(pos,1);
+  
+}
+    console.log(item);
+  });
+  main();
+  
+//console.log("deleteName been called")
 }
 function display() {
-
+  database.forEach((entry) => {
+    console.log(entry);
+  });
+ 
+console.log(database);
+main();
 }
 //starting point of the app
 function main(){
-  readline.setDefaultOptions({ limit: ['green', 'yellow', 'red'] });
-  readline.keyIn
+menu = ['add', 'delete','display'],
+index = readlineMenu.keyInSelect(menu,  "please make a choice:");
+
+
+switch(menu[index]){
+    case "add":
+      add();
+      break;
+    case "delete":
+      remove();
+      break;
+    case "display":
+      display();
+      break;
+    case 0:
+      //code cancel
+      break;
+  }
 }
+
+
 main();

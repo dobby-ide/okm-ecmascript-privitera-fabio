9c741a54f5cc30a497368ed702ae5abef0a80e77
dobby-ide <fabio_privitera@yahoo.it>
2021-09-21 08:32:01 +0300

    using ternary operator

--- homework/2021-36/e03/index.js
+++ homework/2021-36/e03/index.js
@@ -1,8 +1,11 @@
-function max(num1, num2){
-  if(num1>num2){
-    return num1
-  } else{
-    return num2;
-  }
-}
+// function max(num1, num2){
+//   if(num1>num2){
+//     return num1
+//   } else{
+//     return num2;
+//   }
+// }
+const max = (a, b) => {
+  return a > b ? a : b;
+};
 console.log(max(4,6));

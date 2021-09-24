42ed5884036ec6c26e286cd348858535e8ae2b4d
dobby-ide <fabio_privitera@yahoo.it>
2021-09-24 22:55:52 +0300

    to be reviewed

---
+++ homework/2021-38/e18-e22/e22.js
@@ -0,0 +1,16 @@
+
+
+function Person(name) {
+  this.name = name;
+  this.sayHello = function () {
+    console.log(`Hello, ${this.name}!`);
+  };
+  this.sayDelayedHello = function () {
+    setTimeout(this.sayHello, 1000);
+  };
+  this.sayHello = this.sayHello.bind(this); //great explanation on lecture!
+}
+let jack = new Person('jack');
+jack.sayDelayedHello();
+let tina = new Person('tina');
+tina.sayDelayedHello();
